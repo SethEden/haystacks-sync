a7935f90f8b2ef5dc38031aa407fd328b87ec134
Seth Hollingsead <7800783+SethEden@users.noreply.github.com>
2023-04-06 08:57:51 -0500

    unitTest01: Code review and setup the constants system.

--- test/unitTest/tests/businessRules/rules/arrayParsing/auxiliaryArrayParsing.test.js
+++ test/unitTest/tests/businessRules/rules/arrayParsing/auxiliaryArrayParsing.test.js
@@ -3,7 +3,8 @@
  * @file auxiliaryArrayParsing.test.js
  * @module auxiliaryArrayParsing.test
  * @description Unit tests for the auxiliaryArrayParsing.js
- * @requires module:auxiliaryArrayParsing.js
+ * @requires module:auxiliaryArrayParsing
+ * @requires module:test.constants
  * @requires {@link https://www.npmjs.com/package/@haystacks/constants|@haystacks/constants}
  * @requires {@link https://www.npmjs.com/package/jest|jest}
  * @requires {@link https://www.npmjs.com/package/path|path}
@@ -12,30 +13,32 @@
  * @copyright Copyright © 2023-… by Seth Hollingsead. All rights reserved
  */
 
-import { createRequire } from 'node:module';
-import { beforeAll, beforeEach, jest } from '@jest/globals';
-
-const require = createRequire(import.meta.url);
-
 // Internal imports
 import auxiliaryArrayParsing from "../../../../../../src/businessRules/rules/arrayParsing/auxiliaryArrayParsing.js";
-// const auxiliaryArrayParsing = require("../../../../../../src/businessRules/rules/arrayParsing/auxiliaryArrayParsing")
-
-// const auxiliaryArrayParsing = await import('../../../../../../src/businessRules/rules/arrayParsing/auxiliaryArrayParsing.js')
-
+import tst_con from '../../../constants/test.constants.js';
 // External imports
-// import { describe, expect, test } from '@jest/globals'
-// const hayConst = require('@haystacks/constants');
-// const jest = require('jest');
-// const path = requrie('path');
+// import hayConst from '@haystacks/constants';
+import { beforeAll, beforeEach, jest } from '@jest/globals';
 
 // const {bas, msg, sys, wrd} = hayConst;
 // const baseFileName = path.basename(import.meta.url, path.extname(import.meta.url));
 // businessRules.rules.arrayParsing.auxiliaryArrayParsing.
 // const namespacePrefix = sys.cbusinessRules + bas.cDot + wrd.crules + bas.cDot + wrd.carray + wrd.cParsing + bas.cDot + baseFileName + bas.cDot;
 
-describe('parseColorRangeInputs Tests', () => {
-  test('parseColorRangeInputs_validDataString1', async () => {
+/**
+ * @function parseColorRangeInputs
+ * @description Tests the positive and negative test cases of the parseColorRangeInputs function.
+ * @author Seth Hollingsead
+ * @date 2023/04/06
+ */
+describe(tst_con.cparseColorRangeInputs, () => {
+  /**
+  * @function parseColorRangeInputs_validDataInteger1
+  * @description Tests the business rules function parseColorRangeInputs with a valid integer input.
+  * @author Seth Hollingsead
+  * @date 2022/01/21
+  */
+  test(tst_con.cparseColorRangeInputs_validDataInteger1, async () => {
     let returnData = [];
     returnData = auxiliaryArrayParsing.parseColorRangeInputs(1, 10);
     expect(returnData[0]).toBe(1);
---
+++ test/unitTest/tests/constants/test.constants.js
@@ -0,0 +1,19 @@
+/**
+ * @file test.constants.js
+ * @module test.constants
+ * @description Contains many re-usable test constants.
+ * @requires {@link https://www.npmjs.com/package/@haystacks/constants|@haystacks/constants}
+ * @author Seth Hollingsead
+ * @date 2023/04/06
+ * @copyright Copyright © 2023-… by Seth Hollingsead. All rights reserved
+ */
+
+// External imports
+import hayConst from '@haystacks/constants';
+const {bas, num, sys, wrd} = hayConst;
+
+// Describe names
+export const cparseColorRangeInputs = wrd.cparse + wrd.cColor + wrd.cRange + wrd.cInputs;
+
+// Test names
+export const cparseColorRangeInputs_validDataInteger1 = cparseColorRangeInputs + wrd.cvalid + wrd.cData + wrd.cInteger + num.c1;
